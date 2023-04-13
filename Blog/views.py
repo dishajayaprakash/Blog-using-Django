f116c16abe0cd2e9e41d855216c8103250161b56
dishajayaprakash <dishajayaprakash94@gmail.com>
2023-04-13 23:33:00 +0530

    13th april

--- Blog/views.py
+++ Blog/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+posts = [
+    {
+        "author": "Disha",
+        "content": "Life",
+        "title": "Life as we know it",
+        "Posted_Date": "Today",
+    },
+    {
+        "author": "Diya",
+        "content": "Dance",
+        "title": "ABCD",
+        "Posted_Date": "Yesterday",
+    },
+]
 # Create your views here.
 def home_page(request):
-    return HttpResponse("Hello")
+    context = {"posts": posts}
+    return render(request, "blogy.html", context)
 def blog(request):
     return HttpResponse("Blog Home")
