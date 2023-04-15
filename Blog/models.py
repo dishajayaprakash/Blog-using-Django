d155fef0f6919f52feaca8d254f630841aa1d926
dishajayaprakash <dishajayaprakash94@gmail.com>
2023-04-15 12:16:22 +0530

    15th April

--- Blog/models.py
+++ Blog/models.py
@@ -9,6 +9,7 @@ class Posts(models.Model):
     content = models.TextField()
     title = models.CharField(max_length=100)
     Posted_Date = models.DateTimeField(default=timezone.now)
+    
 
     def __str__(self):
         return self.title
